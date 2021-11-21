f54f14b5dc06cffbd0e724b84e747a192848e97c
Gildo Neto <gildoneto@hotmail.com>
2021-11-21 02:07:13 -0300

    FizzBuzz

--- exercicios/01 - fizzBuzz/script.js
+++ exercicios/01 - fizzBuzz/script.js
@@ -5,3 +5,29 @@
 // Se não for um número => 'Não é um número',
 // Se não for divisível nem por 3 e nem por 5 => Entrada
 
+const btnRodar = document.querySelector('#btnRodar')
+
+btnRodar.addEventListener("click", function(e){
+    e.preventDefault();
+});
+
+function printFizzBuzz(){
+    let input = Number(document.querySelector('#usrInput').value)
+    let resultado = fizzBuzz(input)
+    document.querySelector('#resposta').value = resultado
+    console.log(resultado)
+
+}
+
+function fizzBuzz(input) {
+    if (typeof input !== 'number')
+        return 'Não é um número'
+    if ((input % 3 === 0) && (input % 5 === 0))
+        return 'FizzBuzz'
+    if (input % 3 === 0)
+        return 'Fizz'
+    if (input % 5 === 0)
+        return 'Buzz'
+
+    return input
+}
