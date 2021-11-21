94b7ce3e4257925c98fd9f4a036969d9517754c9
Gildo Neto <gildoneto@hotmail.com>
2021-11-21 00:59:20 -0300

    Cria exercicio 01

---
+++ exercicios/01 - fizzBuzz/script.js
@@ -0,0 +1,7 @@
+// FizzBuzz
+// Divisível por 3 => 'Fizz',
+// Divisível por 5 => 'Buzz',
+// Divisível por 3 e 5 => 'FizzBuzz',
+// Se não for um número => 'Não é um número',
+// Se não for divisível nem por 3 e nem por 5 => Entrada
+
